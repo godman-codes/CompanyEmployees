f70924eead9d1896d3df8206d28f25b195241537
Faseun <faseunoluwatobilobasimon@gmail.com>
2023-05-19 00:38:27 +0100

    implemented seraching

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -8,5 +8,6 @@ namespace Shared.RequestFeatures
         public uint MaxAge { get; set; } = int.MaxValue;
 
         public bool ValidAgeRange => MaxAge > MinAge;
+        public string? SearchTerm { get; set; }
     }
 }
